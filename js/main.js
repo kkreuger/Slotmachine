cfb6b7b6854f1a2b4184a214aa9592bb632f8c3a
kkreuger <40921022+kkreuger@users.noreply.github.com>
2020-02-13 20:49:48 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var winning;
 var lightThree;
-var winningSound = new Audio('../sounds/winning.mp3');
-var startSound = new Audio('../sounds/startsound.mp3');
+var winningSound = new Audio('https://raw.githubusercontent.com/kkreuger/slotmachine/master/sounds/winning.mp3');
+var startSound = new Audio('https://raw.githubusercontent.com/kkreuger/slotmachine/master/sounds/startsound.mp3');
 
 function startGame() {
 	var status 		= document.getElementById('status');
